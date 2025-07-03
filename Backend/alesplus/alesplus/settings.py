04a01ea7fc3adcf4251a8b3ed3a5a6ca5c8ae789
Asajadafsar <sajadafsar313@gmail.com>
2025-07-03 19:40:31 +0330

    add files

--- Backend/alesplus/alesplus/settings.py
+++ Backend/alesplus/alesplus/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'  
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -171,6 +172,3 @@ SWAGGER_SETTINGS = {
     'DEFAULT_INFO': 'alesplus.urls.schema_view',
 }
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
