7680f776c528eea61499b5ab7b845bfc96477366
Asajadafsar <sajadafsar313@gmail.com>
2025-07-04 17:04:55 +0330

    set server

--- Backend/alesplus/alesplus/settings.py
+++ Backend/alesplus/alesplus/settings.py
@@ -177,9 +177,10 @@ SWAGGER_SETTINGS = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://api.alecplus.tech",
+    "http://localhost:5173"
 ]
+
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'content-type',
